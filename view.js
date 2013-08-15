c9defac7ad3c78884018e461dbad48a7a3872e15
Daniel Cousens <daniel210x@gmail.com>
2013-08-16 06:49:03 +1000

    Code formatting

--- view.js
+++ view.js
@@ -5,15 +5,15 @@ function TwoView(domElement, model) {
 	var agentVerts = [new Two.Vector(0, 0)];
 
 	// model bindings
+	model.on('despawn', function(agent) { two.remove(agent.__2vo); });
 	model.on('spawn', function(agent) {
 		agent.__2vo = new Two.Polygon(agentVerts, true, false);
 		agent.__2vo.stroke = 'white';
-		agent.__2vo.scale = 5; // TODO: scale based on screensize?
+		agent.__2vo.scale = 5;
 		agent.__2vo.translation.copy(agent.position);
 
 		two.add(agent.__2vo);
 	});
-	model.on('despawn', function(agent) { two.remove(agent.__2vo); });
 	model.on('step', function(dt) {
 		_.each(model.world.agents, function(agent) {
 			agent.__2vo.translation._x = agent.position.x;
